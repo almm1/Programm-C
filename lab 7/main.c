fb960e965d75580def3feda9ace47c39340a8777
almm1 <47571124+almm1@users.noreply.github.com>
2019-06-05 23:41:59 +0300

    Update main.c

--- lab 7/main.c
+++ lab 7/main.c
@@ -1,43 +1,28 @@
 #include <stdio.h> 
 
+void two(int N)
+{
+	for (int i = 31; i >= 0; --i)
+	{
+		printf("%d", (N & (1 << i)) ? 1 : 0);
+		if (i % 8 == 0)
+			printf(" ");
+	}
+	printf("\n");
+}
+
 int main()
 {
-	int x = 0;
-	int temp = 0;
-	int mask = 0b0;
-	int d = 4;
-	unsigned int res_x = 0;
-
+	unsigned long x = 0;
 	printf("x = ");
-	scanf_s("%x", &x);
-	res_x = (unsigned int)x;
+	scanf("%d", &x);
 
-	for (int i = 0; i < sizeof(res_x) * 8; i++)
-	{
-		temp = res_x;
-		res_x <<= 1;
-		res_x >>= 1;
-
-		if (res_x == temp)
-		{
-			mask |= (0 << 31 - i - d);
-		}
-		else
-		{
-			mask |= (1 << 31 - i - d);
-		}
-		if ((i + 1) % 4 == 0)
-		{
-			d = d - 8;
-		}
-		if ((i + 1) % 8 == 0)
-		{
-			d = 4;
-		}
-		res_x <<= 1;
-	}
-
-	printf("x = %x\n", mask);
+	two(x);
+	x = (x & 0xAAAAAAAA) >> 1 | (x & 0x55555555) << 1;
+	two(x);
+	printf("x = %d", x);
+	return 0;
+}
 
 	return 0;
 }
